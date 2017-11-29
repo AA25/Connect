4468ba5b5c18e02d866898ff4fb272a9cac163a3
AA25 <aa25@live.ie>
2017-11-29 23:16:11 +0000

    just added some new html pages for later

--- dashboard/includes/businessSidebar.php
+++ dashboard/includes/businessSidebar.php
@@ -1,6 +1,8 @@
 <div id="dashboardSidebarOptions" class="" style="background-color:none">
     <h3>Business View SideBar</h3>
     <ul>
-        <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','projectRequests')">Project Requests</li>
+        <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','')">Start your project</li>
+        <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','projectDevelopers')">The developers on your projects</li>
+        <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','projectRequests')">Current project requests</li>
     </ul>
 </div>
