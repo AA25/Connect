3d2e1460628a061883cbb4c7ddf74e98c0f8c031
AA25 <aa25@live.ie>
2017-11-29 16:31:49 +0000

    created api that returns business profile details based on username and js file that receives it

---
+++ js/businessProfile.js
@@ -0,0 +1,25 @@
+function retrieveBusinessDetails() {
+    var url = '../api/endpoints/retrieveBusiness.php' + location.search;
+    $.ajax({
+        url: url,
+        data: {},
+        type: 'get',
+        method: 'GET',
+        beforeSend: function(request) {
+            request.setRequestHeader('Authorization', 'Bearer ' + getCookie('JWT').replace(" ", "+"));
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            //Error in setting status
+        },
+        success: function(response) {
+            if (response['Error']) {
+                console.log(response['Error']);
+            } else {
+                console.log(response);
+                //sideDisplay(response['userType']);
+            }
+        }
+    });
+};
+
+window.onload = retrieveBusinessDetails();
