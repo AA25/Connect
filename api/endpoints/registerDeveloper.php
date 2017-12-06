65f34dd48fd2387897a40814701bdc2e3682711a
AA25 <aa25@live.ie>
2017-12-06 13:44:28 +0000

    added server validation object and methods that do serverside validation and sanitisation and integrated into the registerdev api

--- api/endpoints/registerDeveloper.php
+++ api/endpoints/registerDeveloper.php
@@ -7,18 +7,34 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+//This is the data that was inside the body of the request sent
 $registerJSON = json_decode(file_get_contents('php://input'),true);
 
-if(!empty($registerJSON['firstName']) && !empty($registerJSON['lastName'])){
-    //http://thisinterestsme.com/php-pdo-transaction-example/
+$validationCheck = new ServerValidation();
+
+$validationCheck->registerDeveloperSanitisation($registerJSON['firstName'],$registerJSON['lastName'],$registerJSON['dob'],
+$registerJSON['languages'],$registerJSON['email'],$registerJSON['password'],$registerJSON['devBio'],
+$registerJSON['phone'],$registerJSON['username']);
+
+
+
+//If the serside validation passes then proceed with the inseration of the project
+if($validationCheck->registerDeveloperSanitisation(
+    $registerJSON['firstName'],$registerJSON['lastName'],$registerJSON['dob'],
+    $registerJSON['languages'],$registerJSON['email'],$registerJSON['password'],$registerJSON['devBio'],
+    $registerJSON['phone'],$registerJSON['username'])){
+//if(!empty($registerJSON['firstName']) && !empty($registerJSON['lastName'])){
+    // Learned how to use transactions from http://thisinterestsme.com/php-pdo-transaction-example/
     //We start our transaction.
     $pdo->beginTransaction();
     try{
+        //Insert user data into the database
         $r = $pdo->prepare(
             "insert into
             developers (firstName, lastName, dob, languages, email, password, devBio, phone, type, username)
             values(:firstName, :lastName, :dob, :languages, :email, :password, :devBio, :phone, :type, :username);"
           );
+
         $r->execute([
             'firstName' => $registerJSON['firstName'],
             'lastName' => $registerJSON['lastName'],
@@ -45,6 +61,6 @@ if(!empty($registerJSON['firstName']) && !empty($registerJSON['lastName'])){
         $pdo->rollBack();
     }
 }else{
-    echo json_encode(array('Error' => 'Empty Fields'));
+    echo json_encode(array('Error' => 'Validation failed'));
 }
 ?>
---
+++ classes/serverValidation.php
@@ -0,0 +1,60 @@
+<?php 
+    //Object that contains validation methods for each form on the site as well as sanitisation methods to sanitise data before inserting into the database
+    //Testers within my tribe helped with validation and security
+    //Got some basic ideas of php form validation from https://www.w3schools.com/php/php_form_validation.asp
+    class ServerValidation{
+
+        public function sanitisation($formData){
+            $formData = trim($formData);
+            $formData = stripslashes($formData);
+            $formData = htmlspecialchars($formData);
+            return $formData;
+        }
+
+        public function registerDeveloperSanitisation(&$firstName,&$lastName,&$dob,&$languages,&$email,&$password,&$devBio,&$phone,&$username){
+            $firstName      = $this->sanitisation($firstName);
+            $lastName       = $this->sanitisation($lastName);
+            $dob            = $this->sanitisation($dob);
+            $languages      = $this->sanitisation($languages);
+            $password       = $this->sanitisation($password);
+            $devBio         = $this->sanitisation($devBio);
+            $phone          = $this->sanitisation($phone);
+            $username       = $this->sanitisation($username);
+
+            return $this->registerDeveloperValidation($firstName,$lastName,$dob,$languages,$email,$password,$devBio,$phone,$username);
+        }
+
+        public function registerDeveloperValidation($firstName,$lastName,$dob,$languages,$email,$password,$devBio,$phone,$username){
+
+            if( !(ctype_alpha($firstName)) || (strlen($firstName) >56) || empty($firstName) ){
+                return false;
+            }
+            if( !(ctype_alpha($lastName)) || (strlen($firstName) >56) || empty($firstName)){
+                return false;
+            }
+            if( strtotime($dob) == '' || empty($dob) ){
+                return false;
+            }
+            if( (strlen($languages) > 500) || empty($languages) ){
+                return false;
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL) || empty($email) ) {
+                return false;
+            }
+            if( (strlen($password) > 500) || empty($password) ){
+                return false;
+            }
+            if( (strlen($devBio) > 500) || empty($devBio) ){
+                return false;
+            }
+            if( (strlen($phone) > 45) || empty($phone) ){
+                return false;
+            }
+            if( (strlen($username) > 100) || empty($username) ){
+                return false;
+            }
+
+            return true;
+        } 
+    }
+?>
