26ac67f8fa2ddccbcdedb1095d49904926c02688
AA25 <aa25@live.ie>
2017-12-17 16:43:47 +0000

    basic html front end for developers and dynamic

--- controllers/js/developerProfileController.js
+++ controllers/js/developerProfileController.js
@@ -1,7 +1,5 @@
 function retrieveProjectDetails() {
-    ///developer/info/restletdev
     var url = window.location.pathname;
-    //url = url.replace(new RegExp(/-/g), ".");
     $.ajax({
         url: '/api' + url,
         data: {},
@@ -17,11 +15,41 @@ function retrieveProjectDetails() {
             if (response['Error']) {
                 console.log(response['Error']);
             } else {
-                console.log(response);
-                //sideDisplay(response['userType']);
+                console.log(response['Success'][0]);
+                renderContent(response['Success'][0]);
             }
         }
     });
 };
+
+function renderContent(profileData) {
+    profileData['dob'] = userAge(profileData['dob']);
+
+    $("#name").text(profileData['firstName'] + ' ' + profileData['lastName']);
+    $("#age").text(profileData['dob']);
+    $("#languages").text(profileData['languages']);
+    $("#phone").text(profileData['phone']);
+    $("#email").text(profileData['email']);
+    $("#profileDescription").text(profileData['devBio']);
+    console.log(profileData['currentProject']);
+    if (profileData['currentProject'] !== null) {
+        $("#projectStatus").text(profileData['firstName'] + ' is currently part of a project and unavailable');
+        $("#projectStatus").addClass("cl-danger");
+    } else if (profileData['currentProject'] === null) {
+        $("#projectStatus").text(profileData['firstName'] + ' is currently available to join a project');
+        $("#projectStatus").addClass("cl-success");
+    }
+};
+
+//Function to calculate the age of the user
+function userAge(dob) {
+    dob = dob.split("-");
+    var today = new Date();
+    var date = new Date(dob[0], dob[1], dob[2], 0, 0, 0);
+    var userAge = (today - date) / (1000 * 60 * 60 * 24 * 365);
+    userAge = Math.floor(userAge);
+
+    return userAge;
+}
 
 window.onload = retrieveProjectDetails();
--- views/developer.php
+++ views/developer.php
@@ -1,30 +1,85 @@
-<? 
-//TODO Introduce bootstrap js locally instead of a externallink
-?>
-
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Connect Project Description</title>
-        <link rel="stylesheet" type="text/css" href="/css/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="/css/font-awesome-4.7.0/css/font-awesome.min.css"/>
-        <link rel="stylesheet" type="text/css" href="/css/connectStyle.css"/>
-    </head>
-    <body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connect Project Description</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/font-awesome-4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/css/connectStyle.css"/>
+</head>
+<body id="top">
 
-    <div>
-        Developer page
+    <?php include('./includes/navBar.inc.php')?>
+
+    <div id="developerProfileContent" class="container padt-56 min-h-770">
+        <div class="padt-20 padb-20">
+            <div class="row">
+                <div class="col-lg-3">
+                    <img src="/images/blankProfile.png" alt="" class="bord-rd" width="210" height="205">
+                </div>
+                <div class="col-lg-6">
+                    <h3 id="name" class="cl-blue-connect"></h3>
+                    <h5>Developer</h5>
+                    <div class="row padt-10">
+                        <div class="col-lg-6">
+                            <p>
+                                <span class="cl-blue-connect">Age:</span>
+                                <br>
+                                <span id="age"></span>
+                            </p>
+                        </div>
+                        <div class="col-lg-6">
+                            <p>
+                                <span class="cl-blue-connect">Languages Spoken:</span>
+                                <br>
+                                <span id="languages"></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row padt-10">
+                        <div class="col-lg-6">
+                            <p>
+                                <span class="cl-blue-connect">Phone:</span>
+                                <br>
+                                <span id="phone"></span>
+                            </p>
+                        </div>
+                        <div class="col-lg-6">
+                            <p>
+                                <span class="cl-blue-connect">Email:</span>
+                                <br>
+                                <span id="email"></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-9 padt-20">
+                    <h5 class="cl-blue-connect">Profile Description</h5>
+                    <p id="profileDescription">
+                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h5 id="projectStatus" class="marb-0"></h5>
+                </div>
+            </div>
+        </div>
     </div>
 
-    </body>
-    <script src="/controllers/js/jQuery/jquery.min.js"></script>
-    <script src="/controllers/js/tether/tether.min.js"></script>
-    <script src="/controllers/js/bootstrap/bootstrap.min.js"></script>
-    <script src="/controllers/js/smoothScroll/smoothScroll.js"></script>
-    <script src="/controllers/js/navBar.js"></script>
-    <script src="/controllers/js/developerProfileController.js"></script>
+    <?php include('./includes/footer.inc.php')?>
+
+</body>
+<script src="/controllers/js/jQuery/jquery.min.js"></script>
+<script src="/controllers/js/tether/tether.min.js"></script>
+<script src="/controllers/js/bootstrap/bootstrap.min.js"></script>
+<script src="/controllers/js/smoothScroll/smoothScroll.js"></script>
+<script src="/controllers/js/navBar.js"></script>
+<script src="/controllers/js/developerProfileController.js"></script>
 </html>
 
