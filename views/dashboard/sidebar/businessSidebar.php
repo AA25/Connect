3975ac43a3c0b16ec0bec57ef697bf6f1446dcb1
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2017-12-21 13:40:57 +0000

    addedd profile link on busienss dashboard

--- views/dashboard/sidebar/businessSidebar.php
+++ views/dashboard/sidebar/businessSidebar.php
@@ -45,17 +45,13 @@
                 </p>
             </div>
         </div>
-        <!-- <ul>
-            <li>
-                <a href="http://localhost:8081/dashboard/register/project" target="_blank"> Register A New Project</a>
-            </li>
-            <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','beginProjectJourney')">Begin Project Journey</li>
-            <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','projectRequests')">Current Project Requests</li>
-            <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','projectDevelopers')">Your Project Developers</li>
-            <li onclick="renderSidebarOption('<?php echo $userVerifiedData['type'] ?>','manageProjects')">Manage Your Projects</li>
-            <li>
-                <a href="http://localhost:8081/business/info/<?php echo str_replace(".","-",$userVerifiedData['username']); ?>" target="_blank">Your Profile</a>
-            </li>
-        </ul> -->
+
+        <div class="row" style="border-bottom: 1px solid black;">
+            <div class="col-sm-12 padl-30">
+                <p class="marb-15 mart-15">
+                    <a class="cl-black-connect" href="http://localhost:8081/business/info/<?php echo str_replace(".","-",$userVerifiedData['username']); ?>" target="_blank"> My Profile</a>
+                </p>
+            </div>
+        </div>
 
 </div>
