d5ea1af9f534a244f03a31190010dda59e990ad5
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2017-12-29 21:03:03 +0000

    added comments on navbar controller and have alerts coming up now

--- controllers/js/navbarController.js
+++ controllers/js/navbarController.js
@@ -1,36 +1,47 @@
 //When the login button is clicked, an ajax request is made to check if the details is correct
 $('#loginForm').submit(function(e) {
     e.preventDefault();
+    //Get the inputted values from the login form
     var data = {
-        'email': $('#loginForm input[name=email]').val(),
-        'password': $('#loginForm input[name=password]').val(),
-        'location': $('#loginForm select[name=location] option:selected').val()
-    }
+            'email': $('#loginForm input[name=email]').val(),
+            'password': $('#loginForm input[name=password]').val(),
+            'location': $('#loginForm select[name=location] option:selected').val()
+        }
+        //Make an ajax request to the login controller php file
     $.ajax({
         url: "/controllers/php/loginController.php",
         data: JSON.stringify(data),
         type: 'post',
         method: 'POST',
-        error: function formError(XMLHttpRequest, textStatus, errorThrown) {
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
             //Error in setting status
-            console.log("error ajax post to checklogin");
+            //console.log("error ajax post to checklogin");
         },
-        success: function formSuccess(response) {
-            location.reload();
+        success: function(response) {
+            if (response['Error']) {
+                //On fail error show error alert
+                errorDisplay(response['Error']);
+            } else if (response['Success']) {
+                //On success reload the page
+                location.reload();
+            }
         }
     });
 });
 
-function successDisplay(response) {
-    console.dir(response);
-}
-
 function errorDisplay(response) {
-    console.dir(response);
+    //Show the container containing alerts
+    $("#alertContainer").show();
+    //Attach the response to the alert
+    $(".alertError p").text(response);
+    //Display the specific alert
+    $(".alertError").show();
+    //Remove alert after 2 seconds
+    setTimeout(function() { $(".alertError").alert('close') }, 2000);
 }
 
 function logOut() {
-    location.reload();
+    //Make an ajax request to the logout controller php file
     $.ajax({
         url: "/controllers/php/logoutController.php",
         data: {},
@@ -38,7 +49,7 @@ function logOut() {
         method: 'GET',
         error: function formError(XMLHttpRequest, textStatus, errorThrown) {
             //Error in setting status
-            console.log("error ajax post to checklogin");
+            //console.log("error ajax post to checklogin");
         },
         success: function formSuccess(response) {
             //Cookie containing token has been deleted, now refresh page
@@ -47,7 +58,9 @@ function logOut() {
     });
 }
 
-//Function to get a specific cookie 
+//Function to get a specific cookie
+//For certain requests the login token must be attached with it so we get the the cookie that contains the token
+//and attach it to the ajax requests
 //https://www.w3schools.com/js/js_cookies.asp
 function getCookie(cname) {
     var name = cname + "=";
@@ -68,113 +81,3 @@ function getCookie(cname) {
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
-
-
-// var getUrl = window.location;
-// var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-// console.log(baseUrl);
-
-// function formData(formLocation, searchLocation) {
-//     var formData = {
-//         'location': searchLocation,
-//         'email': $('#' + formLocation + ' input[name=email]').val(),
-//         'password': $('#' + formLocation + ' input[name=password]').val()
-//     };
-//     return formData;
-// }
-
-//When the login button is clicked, an ajax request is made to check if the details is correct
-// $('#loginDevForm').submit(function(e) {
-//     e.preventDefault();
-//     var data = formData('loginDevForm', 'developers');
-//     $.ajax({
-//         url: "./logic/loginUser.php",
-//         data: JSON.stringify(data),
-//         type: 'post',
-//         method: 'POST',
-//         error: function formError(XMLHttpRequest, textStatus, errorThrown) {
-//             //Error in setting status
-//             console.log("error ajax post to checklogin");
-//             //console.log(textStatus);
-//         },
-//         success: function formSuccess(response) {
-//             //response = JSON.parse(response);
-//             console.log(response);
-//             //response['Success'] ? decodeJWT(response['Success']) : console.log(response);
-
-//             //response = JSON.parse(atob(response));
-//             //response['Success'] ? successProcedure(response) : errorProcedure(response);
-//         }
-//     });
-// });
-
-// $('#loginBusForm').submit(function(e) {
-//     e.preventDefault();
-//     var data = formData('loginBusForm', 'businesses');
-//     $.ajax({
-//         url: "./logic/loginUser.php",
-//         data: JSON.stringify(data),
-//         type: 'post',
-//         method: 'POST',
-//         error: function formError(XMLHttpRequest, textStatus, errorThrown) {
-//             //Error in setting status
-//             console.log("error ajax post to checklogin");
-//             console.log(textStatus);
-//         },
-//         success: function formSuccess(response) {
-//             console.log(response);
-//             //response = JSON.parse(atob(response));
-//             //response['Success'] ? successProcedure(response) : errorProcedure(response);
-//         }
-//     });
-// });
-
-//This isnt needed anymore?
-// function testRequest(){
-//     $.ajax({
-//         url: "./views/testView.php",
-//         beforeSend: function(request){
-//             request.setRequestHeader('Authorization', 'Bearer ' + localStorage.JWT);
-//         },
-//         type: 'GET',
-//         success: function(data) {
-//             // Decode and show the returned data nicely.
-//         },
-//         error: function() {
-//             alert('error');
-//         }
-//     });
-// }
-
-//This isnt needed anymore?
-// function decodeJWT(jwtToken){
-//     //jwtToken = "eyJhbGciOiAiSFMyNTYiLCJ0eXAiOiAiSldUIn0=.eyJTdWNjZXNzIjoiU3VjY2Vzc2Z1bCBsb2dpbiIsImZpcnN0TmFtZSI6ImFkZWUiLCJsYXN0TmFtZSI6ImtpbmciLCJkb2IiOiIxOTk0LTA2LTI1IiwibGFuZ3VhZ2VzIjoiZW5nbGlzaCIsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImRldkJpbyI6ImJpbyIsInBob25lIjoiMSJ9.ELfHZXsMOcoH4XJzaF658t8KIlINqb1mCsIA0zvTVK8=";
-//     //Break the JWT into its three parts, header, payload and signature
-//     tokenParts = jwtToken.split(".");
-//     jwtHeaderEnc = tokenParts[0];
-//     jwtPayloadEnc = tokenParts[1];
-//     jwtSigEnc = tokenParts[2];
-
-//     jwtPayloadDec = JSON.parse(atob(jwtPayloadEnc));
-//     //console.log(jwtPayloadDec);
-//     setCookieUser(jwtToken,jwtPayloadDec);
-// }
-//decodeJWT("eyJhbGciOiAiSFMyNTYiLCJ0eXAiOiAiSldUIn0=.eyJTdWNjZXNzIjoiU3VjY2Vzc2Z1bCBsb2dpbiIsImZpcnN0TmFtZSI6ImFkZWUiLCJsYXN0TmFtZSI6ImtpbmciLCJkb2IiOiIxOTk0LTA2LTI1IiwibGFuZ3VhZ2VzIjoiZW5nbGlzaCIsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImRldkJpbyI6ImJpbyIsInBob25lIjoiMSJ9.ELfHZXsMOcoH4XJzaF658t8KIlINqb1mCsIA0zvTVK8=");
-
-//This isnt needed anymore?
-// function setCookieUser(itemA, itemB){
-//     // if (typeof(Storage) !== "undefined") {
-//     //     // Code for localStorage/sessionStorage.
-//     //     localStorage.setItem("JWT", itemA);
-//     //     for(var info in itemB){
-//     //         //console.log(itemB[info]);
-//     //         localStorage.setItem(info,itemB[info]);
-//     //     } 
-//     // } else {
-//     //     // Sorry! No Web Storage support..
-//     // }
-//     document.cookie = "JWT="+itemA+";";
-//     for(var info in itemB){
-//         document.cookie = info + "=" + itemB[info] + ";";
-//     } 
-// }
