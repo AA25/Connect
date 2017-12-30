473bb6a20876b6859512e342c3cc71cedf76f43a
Ademola Akingbade <ademola.akingbade@skybettingandgaming.com>
2017-12-30 12:15:36 +0000

    more comments

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
+    // All requests to the api are first routed to this file
+
     require "../includes/init.inc.php";
     // Requests from the same server don't have a HTTP_ORIGIN header
     if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
@@ -6,7 +8,9 @@
     }
 
     try {
+        //myAPI object is then created based on the request
         $API = new MyAPI($_SERVER['REQUEST_URI'], $_SERVER['HTTP_ORIGIN']);
+        //check to see if the rest url trying to be accessed exists and return the appropriate response
         echo $API->processAPI();
     } catch (Exception $e) {
         echo json_encode(Array('error' => $e->getMessage()));
